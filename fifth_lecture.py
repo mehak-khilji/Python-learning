85482b6349e6514f4ddfcf25841ae01c1cbf1f9f
mehakkhilji3 <mehakkhilji90@gmail.com>
2025-08-22 08:31:12 +0500

    while loop done

--- fifth_lecture.py
+++ fifth_lecture.py
@@ -1,1 +1,44 @@
-# LOOPS
+# LOOPS IN PYTHON
+count = 1
+while count <= 5 :
+    print("hello")
+    count += 1
+
+    print(count)
+
+# to print for 100 times
+i = 1
+while i <= 100 :
+        print("khilji", i)
+        i += 1
+
+# print number 1 to 5
+i = 1
+while  i <= 5 :
+    print(i)
+    i += 1
+
+print ("loop ended")
+
+# loop in reverse order
+i = 5
+while  i >= 1:
+    print(i)
+    i -= 1
+
+print ("loop ended")
+
+# practice question
+# 1: print numbers from 1 to 100
+
+i = 1
+while i <= 100 :
+    print(i)
+    i += 1
+
+# 2: print numbers from 100 to 1
+
+i = 100
+while i >= 1:
+    print (i)
+    i -= 1
