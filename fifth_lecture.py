0d5e31692361647c8e94bb137d43889e1c19dee3
mehakkhilji3 <mehakkhilji90@gmail.com>
2025-08-23 17:39:11 +0500

    lecture 5 done

--- fifth_lecture.py
+++ fifth_lecture.py
@@ -222,3 +222,70 @@ for i in range(2, 100, 2):
 
 for i in range(1, 100, 2):
     print(i)
+
+# PRACTICE QUESTION USING FOR ANG RANGE()
+
+# 1: print number 1 to 100
+
+for i in range(1, 101):
+    print(i)
+
+# 2: print numbers from 100 to 1
+
+for i in range(100, 0, -1):
+    print(i)
+
+# print multiplication table of n
+
+n = int(input("enter number: "))
+
+for i in range(1, 11):
+    print(n*i)
+
+# PASS STATEMENT (null statement)
+
+for i in range(5):
+    pass
+
+print("some useful info")
+
+# pass statement is also used in if statements
+
+if i > 5:
+    pass
+
+# PRACTIICE QUESTIONS 
+# 1 : program to find the sum of first n natural numbers(using while)
+n = 7
+sum = 0
+i = 1
+while i<= n:
+    sum += i
+    i += 1
+print(sum)
+
+# using for loop
+    
+n = 5
+sum = 0
+for i in range(1, n+1):
+    sum += i
+print("total sum =", sum)
+
+# 2: program to find the factorial of first n natural number (using for)
+
+n = 5
+fact = 1
+for i in range(1, n+1):
+    fact *= i
+
+print("factorial of n =", fact)
+
+# using while
+n = 5
+fact = 1
+i = 1
+while i<= n:
+    fact *= i
+    i += 1
+print("factorial of n =", fact)            
