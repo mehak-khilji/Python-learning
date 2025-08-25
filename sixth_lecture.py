195dbd2ce95316ba6c4ee58684e9017a08f2e5eb
mehakkhilji3 <mehakkhilji90@gmail.com>
2025-08-25 08:41:04 +0500

    recursion completed

--- sixth_lecture.py
+++ sixth_lecture.py
@@ -106,3 +106,23 @@ def find_function(n):
         print("odd number")
 
 find_function(n)        
+
+# RECURSION
+
+def show(n):
+    if (n == 0):
+        return
+    print(n)
+    show(n-1)
+   
+show(5)
+
+#another example
+#return n!
+
+def fact(n):
+    if(n == 1 or n == 0):
+        return 1
+    return fact(n-1)*n
+    
+print(fact(3))   
