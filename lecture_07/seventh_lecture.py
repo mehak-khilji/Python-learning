d8155f3478a6af721863f963f0ec41252995dcab
mehakkhilji3 <mehakkhilji90@gmail.com>
2025-08-29 16:32:32 +0500

    found even num with i/o

--- lecture_07/seventh_lecture.py
+++ lecture_07/seventh_lecture.py
@@ -48,6 +48,104 @@
 # f = open("sample.txt", "w")
 # f.close
 
-f = open("demo.txt","r+")
-f.write("abc")
-f.close()
+# f = open("demo.txt","r+") #overwrite + read
+# f.write("abc")
+# print(f.read())
+# f.close()
+
+# f = open("demo.txt","w+") #truncate data + read
+# print(f.read())
+# f.write("abc")
+# f.close()
+
+# # WITH SYNTAX
+# with open("demo.txt","r")as f:
+#     data = f.read()
+#     print(data)
+
+# with open("demo.txt","w")as f:
+#     f.write("new data")
+
+# # DELETING A FILE 
+# import os
+
+# os.remove("sample.txt")    
+
+# # practice questions
+# # 1: create a new file "practice .txt" using python. add following data in it
+# # Hi everyone
+# # we are learning file I/O
+# # using java
+# # I like programming in java 
+
+# with open("practice.txt", "w") as f:
+#     f.write("Hi everyone\nwe are learning file I/O\n")
+#     f.write("using java\nI like programming in java")
+    
+# # 2: function that replace all occurrences of "java" with "python" in above file
+
+# with open("practice.txt", "r") as f:
+#     data = f.read()
+
+# new_data = data.replace("java", "Python")
+# print(new_data)
+
+# with open("practice.txt", "w") as f:
+#     f.write(new_data)
+
+
+# # 3: search if the word "learning" exist in the file or not
+# def find_word():
+
+#     word = "learning"
+#     with open("practice.txt", "r") as f:
+#         data = f.read()
+#         if(word in data):
+#             print("found")
+#         else:
+#             print("not found")
+
+# find_word()
+
+# # 4: function to find in which line of the file does the word "learning" occur first. print -1 if not found
+
+# def check_for_line():
+#     word = "learning"
+#     data = True
+#     line_no = 1
+#     with open("practice.txt", "r")as f:
+#         while data:
+#             data = f.readline()
+#             if(word in data):
+#                 print(line_no)
+#                 return
+#             line_no += 1
+
+#     return -1
+# print(check_for_line())     
+
+# # 5: from a file containing number separated by comma, print the count of even number 
+
+# with open("practice.txt", "r") as f:
+#     data = f.read()
+#     print(data)
+
+#     num = ""
+#     for i in range(len(data)):
+#         if(data[i] == ","):
+#             print(int(num))
+#             num = ""
+#         else:
+#             num += data[i]
+
+
+# ANOTHER METHOD               
+count = 0
+with open("practice.txt", "r") as f:
+    data = f.read()
+    
+    numbers = data.split(",")
+    for value in numbers:
+        if(int(value)%2 == 0):
+            count += 1 
+print(count)
