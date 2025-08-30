eca29477b502e068b13a4a4fbd254fae637f703f
mehakkhilji3 <mehakkhilji90@gmail.com>
2025-08-30 20:34:30 +0500

    practice question completed

--- lecture_08/lecture_8.py
+++ lecture_08/lecture_8.py
@@ -86,3 +86,114 @@ print(s3.name, s3.marks)
 # print(s3.college_name)
 
 print(Student.college_name)
+
+# METHODS
+
+class Student:
+    college_name = "GCUF"
+    def __init__(self,name, marks):
+        self.name = name
+        self.marks = marks
+        
+    def welcome(self):
+        print("welcome student", self.name) 
+
+    def get_marks(self):
+        return self.marks     
+    
+s1 = Student("mehak", 96)
+s1.welcome()
+print(s1.get_marks())
+
+# PRACTICE QUESTION 
+# 1:create student class that take name & marks of 3 subject as arguments in constructor then create a method to print average
+class Student:
+    def __init__(self,name,marks):
+        self.name = name
+        self.marks = marks 
+
+    def average(self):
+        sum = 0
+        for value in self.marks:
+            sum +=  value
+        print("hi", self.name, "your average score is:", sum/3)  
+
+s1 = Student("Mehak", [92, 77, 89])
+
+s2 = Student("Sana", [89, 90, 87])
+
+s3 = Student("Arslan", [99, 79, 88])
+
+s1.average()
+s2.average()
+s3.average()
+
+# STATIC METHOD 
+class Student:
+    def __init__(self,name,marks):
+        self.name = name
+        self.marks = marks 
+    @staticmethod
+    def hello():
+        print("hello")
+    def average(self):
+        sum = 0
+        for value in self.marks:
+            sum +=  value
+        print("hi", self.name, "your average score is:", sum/3)  
+
+s1 = Student("Mehak", [92, 77, 89])
+s1.average()
+s1.hello()
+
+# ABSTRACTION & ENCAPSULATION
+# abstraction example
+class Car:
+    def __init__(self):
+        self.accelerater = False
+        self.brake = False
+        self.clutch = False
+
+    def start(self):
+        self.clutch = True
+        self.accelerater = True
+        print("Car started..")
+
+car1 = Car()
+car1.start()        
+
+# PRACTICE QUESTION
+# 1: create account class with 2 attributes- balance & account no. create method for debit, credit and printing balance
+class Account:
+    def __init__ (self, balance, account):
+        self.balance = balance
+        self.account_number = account
+
+    #debit method
+    def debit(self, amount):
+        self.balance -= amount
+        print("Rs.", amount, "was debited")
+        print("total balance = ", self.get_balance())
+
+
+    # credit method
+    def credit(self, amount):
+        self.balance +=  amount
+        print("Rs.", amount,"was credited" )  
+        print("total balance = ", self.get_balance())
+
+
+    def get_balance(self):
+        return self.balance
+        
+
+
+Account1 = Account(10000, 12456)        
+print(Account1.balance)
+print(Account1.account_number) 
+Account1.debit(1000)
+Account1.credit(5000)
+Account1.credit(9000)
+Account1.debit(2000)      
+Account1.credit(1000)
+
