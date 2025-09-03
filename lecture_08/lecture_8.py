4e3fe7de142b23e914893993da9830a8c26d22d5
mehakkhilji3 <mehakkhilji90@gmail.com>
2025-09-03 11:14:10 +0500

    lecture 8 completed

--- lecture_08/lecture_8.py
+++ lecture_08/lecture_8.py
@@ -392,3 +392,74 @@ number3.show()
 number3 = number1 - number2
 number3.show()
 
+# PRACTICE QUESTION
+#1: define circle class to create a circle with radius r using the constructor. define an Area() method of the class which calculate area of circle
+# define a perimeter() method of class which allows you to calculate the perimeter of circle.
+class Circle:
+    def __init__(self,radius):
+        self.radius = radius
+
+    def Area(self):
+        return (22/7) * self.radius**2 
+
+    def Perimeter(self):
+        return 2 * (22/7) * self.radius 
+
+c1 = Circle(21)
+print(c1.Area())
+print(c1.Perimeter())
+
+# 2: define a employee class with attributes role, department & salary. this class also has a show details(method)
+
+class Employee:
+    def __init__(self,role,department,salary):
+        self.role = role
+        self.department = department
+        self.salary = salary
+
+    def showDetails(self):
+        print("role =", self.role)
+        print("department =", self.department)
+        print("salary =", self.salary)
+    
+
+e1 = Employee("Accountant","Finance","60000")
+e1.showDetails()
+
+#create an engineer class that inherits properties from employee & has additional attributes: name & age
+
+class Employee:
+    def __init__(self,role,department,salary):
+        self.role = role
+        self.department = department
+        self.salary = salary
+
+    def showDetails(self):
+        print("role =", self.role)
+        print("department =", self.department)
+        print("salary =", self.salary)
+    
+class Engineer(Employee):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        super().__init__("Enginneer", "IT", "75,000")  
+
+
+engineer1 = Engineer("Ahmed","36")
+engineer1.showDetails()
+
+# create a class called ORDER ehich stores item & its prices. use dunder function __gt__() to convey that:
+# order1>order2 if price of order1 > price of order 2
+
+class Order:
+    def __init__(self, item, price):
+        self.item = item
+        self.price = price
+    def __gt__(self, order2):
+        return self.price > order2.price
+
+order1 = Order("chips", 30)
+order2 = Order("chocolate", 50)
+
+print(order1>order2)
